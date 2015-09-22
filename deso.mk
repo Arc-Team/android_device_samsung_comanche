7e1cbef1fffd836b99bb0445da5790e04d3be907
Anthony Cox <motor-head-1991@hotmail.com>
2015-09-21 21:31:29 -0600

    Inherit common_phone.mk, instead of just common.mk

--- deso.mk
+++ deso.mk
@@ -8,7 +8,7 @@ $(call inherit-product-if/exist, vendor/deso/config/gsm.mk)
 $(call inherit-product, vendor/deso/config/nfc_enhanced.mk)
 
 # Inherit some common deso stuff.
-$(call inherit-product, vendor/deso/config/common.mk)
+$(call inherit-product, vendor/deso/config/common_phone.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=comanche TARGET_DEVICE=comanche BUILD_FINGERPRINT="Samsung/COMANCHE/comanche:4.1.2/JZO54K/I847UCCNJ1:user/release-keys" PRIVATE_BUILD_DESC="comanche-user 4.1.2 JZO54K I847UCCNJ1 release-keys"
 
